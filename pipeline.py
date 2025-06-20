1a64d6eb1e31cda22ee0d1b99f3553a2d4a96802
arkiver <arkiver@protonmail.com>
2025-06-20 14:55:44 +0200

    Version 20250620.01. Use dictionary.goo.ne.jp as Referer header.

--- pipeline.py
+++ pipeline.py
@@ -77,7 +77,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20250619.03'
+VERSION = '20250620.01'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:136.0) Gecko/20100101 Firefox/136.0'
 TRACKER_ID = 'goodictionary'
 TRACKER_HOST = 'legacy-api.arpa.li'
@@ -285,7 +285,8 @@ class WgetArgs(object):
             '--warc-header', 'x-wget-at-project-name: ' + TRACKER_ID,
             '--warc-dedup-url-agnostic',
             '--warc-compression-use-zstd',
-            '--warc-zstd-dict-no-include'
+            '--warc-zstd-dict-no-include',
+            '--header', 'Referer: https://dictionary.goo.ne.jp/'
         ]
         dict_data = ZstdDict.get_dict()
         with open(os.path.join(item['item_dir'], 'zstdict'), 'wb') as f:
